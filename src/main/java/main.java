ee0056f6ca8e44e80106611c11f6c638654ec756
Sim-Pirs <theazx05@gmail.com>
2019-10-08 17:44:39 +0200

    sali salu

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,36 @@
-import org.gnu.glpk.glp_prob;
+import org.gnu.glpk.*;
 
 public class main {
         public static void main(String[] args) {
-            double solutionOptimale = 0;
+            System.out.println( GLPK.glp_version());
+            glp_prob lp;
+            glp_smcp parm;
+            SWIGTYPE_p_int ind;
+            int ret;
+
+            try{
+                //On créé le probleme
+                lp = GLPK.glp_create_prob();
+                System.out.println("Problem created");
+                GLPK.glp_set_prob_name(lp, "P");
+
+                //Colonnes
+                GLPK.glp_add_cols(lp, 4);
+
+                GLPK.glp_set_col_name(lp, 1, "x1");
+                GLPK.glp_set_col_kind(lp, 1, GLPKConstants.GLP_CV); //Type de la colonne
+                GLPK.glp_set_col_bnds(lp, 1, GLPKConstants.GLP_DB, 0, .5); // Bornes inf et sup
+
+
+
+            } catch (GlpkException ex){
+                ex.printStackTrace();
+            }
+        }
+
+
+
+         /*   double solutionOptimale = 0;
             Object colonne;
 
             double[] coef;
@@ -47,6 +75,6 @@ public class main {
 
     public static void resoudreSacADos(){
 
-    }
+    }*/
 
 }
