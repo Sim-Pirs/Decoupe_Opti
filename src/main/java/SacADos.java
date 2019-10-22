be7f2ce3a707be8712081244b85a6c442360c53d
Sim-Pirs <theazx05@gmail.com>
2019-10-22 11:18:38 +0200

    a fininsh

--- src/main/java/SacADos.java
+++ src/main/java/SacADos.java
@@ -16,7 +16,7 @@ public class SacADos {
         result = new ArrayList<>();
     }
 
-    public List<Double> run (){
+    public glp_prob run (){
         SWIGTYPE_p_int ind;
         glp_iocp parm;
         int ret;
@@ -30,15 +30,17 @@ public class SacADos {
         for(int i=1; i <= dual.size(); i++)
         {
             GLPK.glp_set_col_name(s, i, "a" + Integer.toString(i));
-            GLPK.glp_set_col_kind(s, i, GLPKConstants.GLP_CV); //Type de la colonne
+            GLPK.glp_set_col_kind(s, i, GLPKConstants.GLP_IV); //Type de la colonne
             GLPK.glp_set_col_bnds(s, i, GLPKConstants.GLP_LO,0, 0); // Bornes inf et sup
         }
 
         ind = GLPK.new_intArray(dual.size()+1); //stocker les indices
-        for(int k=0; k<dual.size();k++){
+        SWIGTYPE_p_double val = GLPK.new_doubleArray(dual.size() + 1); //stocker les valeurs
+
+
+        for(int k=1; k<=dual.size();k++){
             GLPK.intArray_setitem(ind, k, k );
         }
-        SWIGTYPE_p_double val = GLPK.new_doubleArray(dual.size() + 1); //stocker les valeurs
 
         // Create constraints
         GLPK.glp_add_rows(s, 1);
@@ -46,12 +48,19 @@ public class SacADos {
         GLPK.glp_set_row_name(s, 1, "c"+1);
         GLPK.glp_set_row_bnds(s, 1, GLPKConstants.GLP_DB, 0, 100);
 
-        GLPK.glp_set_mat_row(s,1,dual.size()-1,ind,val);
+        for (int j = 1; j <= dual.size(); j++) {
+            double value= coefConstraint[j-1];
+            GLPK.doubleArray_setitem(val, j,value);
+
+            GLPK.glp_set_mat_row(s,1,dual.size(),ind,val);
+        }
+
+
         GLPK.glp_set_obj_name(s, "z'");
         GLPK.glp_set_obj_dir(s, GLPKConstants.GLP_MAX);
 
-        for(int i = 0; i < coefConstraint.length; i++){
-            GLPK.glp_set_obj_coef(s, i, coefConstraint[i]);
+        for(int i = 0; i < dual.size(); i++){
+            GLPK.glp_set_obj_coef(s, i+1, dual.get(i));
         }
 
         parm = new glp_iocp();
@@ -62,9 +71,6 @@ public class SacADos {
             System.out.println("The problem could not be solved");
         }
 
-        for(int i=1;i<=dual.size();i++){
-            result.add(GLPK.glp_mip_col_val(s,i));
-        }
         for(int i = 0; i < result.size(); i++)
             System.out.println(result.get(i));
 
@@ -75,7 +81,8 @@ public class SacADos {
         GLPK.delete_intArray(ind);
         GLPK.delete_doubleArray(val);
 
-        return result;
+
+        return s;
     }
 
     double calcul(){
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,14 +12,15 @@ VM options path:
  */
 
 public class main {
-        public static void main(String[] args) {
-            glp_prob lp;
+
+    static DecoupeOpti decoupeOpti = new DecoupeOpti();
+    static glp_prob lp;
+    static int  nbCol = 4;
+    static int  nbRows = 4;
+    public static void main(String[] args) {
+
             glp_smcp parm;
             int ret;
-            SWIGTYPE_p_int ind;
-
-            int nbCol = 4;
-            int nbRows = 4;
 
             List <Double> dual = new ArrayList<>();
             List resultat;
@@ -27,7 +28,6 @@ public class main {
             /* init la matrice et la liste des coef
 
             - init matrice de base x1 x2 x3 x4 */
-            DecoupeOpti decoupeOpti = new DecoupeOpti();
 
             ArrayList<Integer> x1 = new ArrayList<Integer>(Arrays.asList(2,0,0,0));
             decoupeOpti.addXjtoXi(x1);
@@ -48,62 +48,11 @@ public class main {
                 decoupeOpti.addCoefXjtoListXi(1);
             }
 
-            /*fin init les coefs*/
-
-     /*       for(int i = 0;i<nbCol; i++){
-                System.out.println("");
-                for(int j=0; j<nbRows; j++){
-                    System.out.print(decoupeOpti.matriceXi.get(i).get(j));
-                }
-            }*/
-
             try{
                 //On créé le probleme
-                lp = GLPK.glp_create_prob();
-                System.out.println("Problem created");
-                GLPK.glp_set_prob_name(lp, "P");
-
-                //Colonnes
-                GLPK.glp_add_cols(lp, nbCol);
-
-                for(int i=1; i <= nbCol; i++)
-                {
-                    GLPK.glp_set_col_name(lp, i, "x" + Integer.toString(i));
-                    GLPK.glp_set_col_kind(lp, i, GLPKConstants.GLP_CV); //Type de la colonne
-                    GLPK.glp_set_col_bnds(lp, i, GLPKConstants.GLP_LO,0,0); // Bornes inf et sup
-                }
-
-                ind = GLPK.new_intArray(nbCol+1); //stocker les indices
-                for(int k=0; k<decoupeOpti.matriceXi.size();k++){
-                    GLPK.intArray_setitem(ind, k, k );
-                }
-                SWIGTYPE_p_double val = GLPK.new_doubleArray(nbCol + 1); //stocker les valeurs
-
-               // Create constraints
-               GLPK.glp_add_rows(lp, nbRows);
-                for(int i = 1; i <= nbRows; i++) {
-                    GLPK.glp_set_row_name(lp, i, "c"+i);
-                    GLPK.glp_set_row_bnds(lp, i, GLPKConstants.GLP_FX, decoupeOpti.valueConstraint[i-1], decoupeOpti.valueConstraint[i-1]);
-
-                    for (int j = 1; j <= decoupeOpti.matriceXi.size(); j++) {
-                        GLPK.doubleArray_setitem(val, j, decoupeOpti.matriceXi.get(i-1).get(j-1));
-                    }
-                    GLPK.glp_set_mat_row(lp,i,decoupeOpti.matriceXi.size()-1,ind,val);
-                }
 
                 while(true) {
-
-                    // Fin des contraintes
-
-                    // fonction objectif z
-
-                    GLPK.glp_set_obj_name(lp, "z");
-                    GLPK.glp_set_obj_dir(lp, GLPKConstants.GLP_MIN);
-
-                    for(int i = 0; i < decoupeOpti.valueConstraint.length; i++){
-                        GLPK.glp_set_obj_coef(lp, i, 1);
-                    }
-
+                    lp=createProb();
                     parm = new glp_smcp();
                     GLPK.glp_init_smcp(parm);
                     ret = GLPK.glp_simplex(lp, parm);
@@ -111,26 +60,27 @@ public class main {
                         System.out.println("The problem could not be solved");
                     }
 
-                    // fin
-
-                    //   GLPK.glp_write_sol(lp, "p");
-                    // GLPK.glp_delete_prob(lp);
-                    // fin fonction objectif z
-
-                    GLPK.delete_intArray(ind);
-                    GLPK.delete_doubleArray(val);
-
-                    // dual :
-
                     for (int i = 1; i <= decoupeOpti.matriceXi.size(); i++) {
-                        dual.add(GLPK.glp_get_row_dual(lp, i));
+                        double value=GLPK.glp_get_row_dual(lp, i);
+                        dual.add(value);
                     }
+
+                    for(int i =0; i<dual.size(); i++)
+                        System.out.println(dual);
+
 
                     SacADos sac = new SacADos(dual);
 
-                    resultat = sac.run();
+                    glp_prob lpsac = sac.run();
+                    GLPK.glp_write_lp(lpsac,null,"sac");
 
-                    if (sac.calcul() <= 1) {
+                    double res = GLPK.glp_mip_obj_val(lpsac);
+                    List<Integer> result= new ArrayList<>();
+                    for(int i=1;i<=dual.size();i++){
+                        result.add((int)GLPK.glp_mip_col_val(lpsac,i));
+                    }
+
+                    if (res <= 1) {
                         break;
                     } else {
                         GLPK.glp_add_cols(lp, 1);
@@ -138,7 +88,7 @@ public class main {
                         GLPK.glp_set_col_name(lp, nbCol, "x" + Integer.toString(nbCol));
                         GLPK.glp_set_col_kind(lp, nbCol, GLPKConstants.GLP_CV); //Type de la colonne
                         GLPK.glp_set_col_bnds(lp, nbCol, GLPKConstants.GLP_LO,0,0); // Bornes inf et sup
-                        decoupeOpti.addXjtoXi(resultat);
+                        decoupeOpti.addXjtoXi(result);
                     }
                 }
 
@@ -151,4 +101,51 @@ public class main {
                 ex.printStackTrace();
             }
         }
+
+        public static glp_prob createProb(){
+            glp_prob lp = GLPK.glp_create_prob();
+            System.out.println("Problem created");
+            GLPK.glp_set_prob_name(lp, "P");
+
+            //Colonnes
+            GLPK.glp_add_cols(lp, nbCol);
+
+            for(int i=1; i <= nbCol; i++)
+            {
+                GLPK.glp_set_col_name(lp, i, "x" + Integer.toString(i));
+                GLPK.glp_set_col_kind(lp, i, GLPKConstants.GLP_CV); //Type de la colonne
+                GLPK.glp_set_col_bnds(lp, i, GLPKConstants.GLP_LO,0,0); // Bornes inf et sup
+            }
+
+            SWIGTYPE_p_int ind = GLPK.new_intArray(nbCol+1); //stocker les indices
+            for(int k=1; k<=decoupeOpti.matriceXi.size();k++){
+                GLPK.intArray_setitem(ind, k, k );
+            }
+            SWIGTYPE_p_double val = GLPK.new_doubleArray(nbCol + 1); //stocker les valeurs
+
+            // Create constraints
+            GLPK.glp_add_rows(lp, nbRows);
+            for(int i = 1; i <= nbRows; i++) {
+                GLPK.glp_set_row_name(lp, i, "c"+i);
+                double bounds=decoupeOpti.valueConstraint[i-1];
+                GLPK.glp_set_row_bnds(lp, i, GLPKConstants.GLP_LO, bounds, 0); /////////
+
+                for (int j = 1; j <= decoupeOpti.matriceXi.size(); j++) {
+                    System.out.println(decoupeOpti.matriceXi);
+                    double value= decoupeOpti.matriceXi.get(i-1).get(j-1);
+                    GLPK.doubleArray_setitem(val, j,value);
+                }
+                GLPK.glp_set_mat_row(lp,i,decoupeOpti.matriceXi.size(),ind,val);
+            }
+            GLPK.glp_set_obj_name(lp, "z");
+            GLPK.glp_set_obj_dir(lp, GLPKConstants.GLP_MIN);
+
+            for(int i = 1; i <= decoupeOpti.valueConstraint.length; i++){
+                GLPK.glp_set_obj_coef(lp, i, 1);
+            }
+            GLPK.delete_intArray(ind);
+            GLPK.delete_doubleArray(val);
+            return lp;
+        }
+
 }
